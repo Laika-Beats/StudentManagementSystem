af4c73ae963cf5d57159b5430edbe773612fd81d
Laika-Beats <laikabeatsdenver@gmail.com>
2021-11-16 16:12:23 -0700

    list students controller

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,8 +1,25 @@
 package net.javaguides.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import net.javaguides.springboot.service.StudentService;
 
 @Controller
 public class StudentController {
+	
+	private StudentService studentService;
 
+	public StudentController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
+	}
+	
+	// handler method to handle list students and return mode and view
+	@GetMapping("/students")
+	public String listStudents(Model model) {
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
+	}
 }
